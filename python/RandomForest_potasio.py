37ade512427d6c49536d82ac6391a66c37ea5554
Bertogu <bertogu@gmail.com>
2020-10-19 14:15:09 +0200

    Update RandomForest_potasio.py

--- python/RandomForest_potasio.py
+++ python/RandomForest_potasio.py
@@ -82,6 +82,30 @@ def FromSpatialite2PandasGeoPandas(_dbIn, _sql, _geoDF = False , _geoField = 'GE
     return selectedFOIs
 
 
+def devuelveConexionSpatialite(_dbIn):
+    '''
+    Connect to spatialite database enabling the spatial module
+
+    Parameters
+    ----------
+    _dbIn : str
+        str with the path of the db.
+
+    Returns
+    -------
+    conexion : connection
+        the connection to the datebase.
+
+    '''
+    
+    conexion = sqlite3.connect(_dbIn)
+    conexion.enable_load_extension(True)
+    conexion.execute("SELECT load_extension('mod_spatialite')") 
+    
+    return conexion
+
+
+
 
 if __name__ == '__main__':
     
@@ -145,11 +169,37 @@ if __name__ == '__main__':
     # Aplico el modelo a los datos de test
     y_pred = regressor.predict(X_test)
     
-    len(y_pred)
+    ErrorAbsolutoMedio_test = metrics.mean_absolute_error(y_test, y_pred) 
+    ErrorCuadradoMedio_test = metrics.mean_squared_error(y_test, y_pred)
+    RMSE_test = np.sqrt(metrics.mean_squared_error(y_test, y_pred))   
+
+
+    sqlLocations = "SELECT COOR_X_ETRS89,COOR_Y_ETRS89,ARCILLA,ARENA,CRAD_UK,ETP,FC_UK,GDD,KSAT_UK,LIBREHELADAS,LIMO,mde_250m,MO,pH_RASTER,PMed_ABRIL,PMed_AGOSTO,PMed_ANUAL,PMed_DICIEMBRE,PMed_ENERO,PMed_FEBRERO,PMed_INVIERNO,PMed_JULIO,PMed_JUNIO,PMed_MARZO,PMed_MAYO,PMed_NOVIEMBRE,PMed_OCTUBRE,PMed_PRIMAVERA,PMed_SEPTIEMBRE,PMed_VERANO,RADIACION,Rug_250,SAT_UK,slope_250m,TIERRA_ARABLE,TMed_ABRIL,TMed_AGOSTO,TMed_DICIEMBRE,TMed_ENERO,TMed_FEBRERO,TMed_JULIO,TMed_JUNIO,TMed_MARZO,TMed_MAYO,TMed_NOVIEMBRE,TMed_OCTUBRE,TMed_SEPTIEMBRE,TMMAX_ABRIL,TMMAX_AGOSTO,TMMAX_DICIEMBRE,TMMAX_ENERO,TMMAX_FEBRERO,TMMAX_JULIO,TMMAX_JUNIO,TMMAX_MARZO,TMMAX_MAYO,TMMAX_NOVIEMBRE,TMMAX_OCTUBRE,TMMAX_SEPTIEMBRE,WP_UK FROM LOCATION_COVARIANTS"
+    
+    # Cargo los datos
+    pd_rdmForest_locations = FromSpatialite2PandasGeoPandas(dbIn, sqlLocations, False)
+    
+    pd_rdmForest_locations.columns
+    
+    Covar_CyL = pd_rdmForest_locations.loc[:,:].values
+    
+    k_pred_CyL = regressor.predict(Covar_CyL)
     
     
+    k_pred_CyL2db = pd_rdmForest_locations.loc[:,['COOR_X_ETRS89','COOR_Y_ETRS89']]
+    k_pred_CyL2db['K_PREDICT_RF'] = k_pred_CyL
+    
+    k_pred_CyL2db.loc[:,'K_PREDICT_RF'].describe()
     
     
+    k_pred_CyL2db.to_sql('RF_POTASIO_COOR', devuelveConexionSpatialite(dbIn), if_exists='replace', index=False)
+    
+
+
+
+
+
+
 
 
 
